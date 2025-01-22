441c7e8db1565b79832d0bece8daf9051628c462
Jake Sills <jsills2592@gmail.com>
2025-01-22 15:52:57 -0500

    Added markdown to the editor text.

--- src/MarkdownPreviewer/MarkdownPreviewer.js
+++ src/MarkdownPreviewer/MarkdownPreviewer.js
@@ -3,7 +3,35 @@ import { marked } from 'marked';
 import "./MarkdownPreviewer.css";
 
 const MarkdownPreviewer = () => {
-  const [editorText, setEditorText] = useState('');
+    const [editorText, setEditorText] = useState(`# This is an H1 header
+## This is an H2 Subheader
+You can also make text **bold**... **VERY NICE**!
+Or _italic_.
+Or... wait for it... **_both!_**
+You can strike something out ~~STRIKEOUT~~.
+Here is some \`inline code\`.
+\`\`\`
+function sayHello() {
+    console.log("Hello, world!");
+}
+\`\`\`
+Here is a [link](https://www.freecodecamp.org) 
+> This is a blockquote!
+- This is a list
+    - open circle
+        - filled in square
+1. Order list
+2. ~~Strike TWO~~
+
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+
+`);
+
+marked.setOptions({
+    breaks: true
+});     
 
   const handleChange = (e) => {
     setEditorText(e.target.value);
@@ -17,9 +45,13 @@ const MarkdownPreviewer = () => {
         onChange={handleChange} 
         placeholder="Enter markdown here..." 
       />
-      <div id="preview" dangerouslySetInnerHTML={{ __html: marked(editorText) }} />
+      <div id="preview" dangerouslySetInnerHTML={{ __html: marked(editorText) }}>
+        
+
+      </div> 
 
     </div>
+    
   );
 };
 
