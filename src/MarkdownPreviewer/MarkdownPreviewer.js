3c5aae036c397641fb9d093c72900946f3608800
Jake Sills <jsills2592@gmail.com>
2025-01-22 14:12:44 -0500

    Created JS and CSS for Markdown Previewer

---
+++ src/MarkdownPreviewer/MarkdownPreviewer.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import * as marked from 'marked';
+import "./MarkdownPreviewer.css";
+
+const MarkdownPreviewer = () => {
+  const [editorText, setEditorText] = useState('');
+
+  const handleChange = (e) => {
+    setEditorText(e.target.value);
+  };
+
+  return (
+    <div>
+      <textarea 
+        id="editor" 
+        value={editorText} 
+        onChange={handleChange} 
+        placeholder="Enter markdown here..." 
+      />
+      <div id="preview" dangerouslySetInnerHTML={{ __html: marked(editorText) }} />
+
+    </div>
+  );
+};
+
+export default MarkdownPreviewer;
